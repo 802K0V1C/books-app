9123580e875d2f32c9328598e2a2ad671f792b1a
Artur Bożek <arturhubert.bozek@gmail.com>
2023-04-19 11:44:23 +0200

    10.2 cwiczenie 4,5

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const template = Handlebars.compile(document.querySelector('#template-book').inn
 const booksList = document.querySelector('.books-list');
 // console.log(booksList);
 
-function render() {
+function render(){
 
   // eslint-disable-next-line no-undef
   for (let book of dataSource.books) {
@@ -14,18 +14,20 @@ function render() {
 }
 render();
 
-const favoriteBooks = [];
-
 function initActions(){
+  const thisBookList = this;
+
+  const favoriteBooks = [];
 
   const booksImage = document.querySelectorAll('.book__image');
 
-  for(let image of booksImage){
-
+  for(let image of thisBookList.booksImage){
     image.addEventListener('dbclick', function(event){ 
       event.preventDefault();
 
-      const bookId = image.getAttribute('data-id'); 
+      event.target.classList.toggle('favorite');
+
+      const bookId = event.target.getAttribute('data-id'); 
 
       if(!favoriteBooks.includes(bookId)){ 
         favoriteBooks.push(bookId);  
@@ -36,6 +38,46 @@ function initActions(){
       }
     });
   }
-}
+  
+  const filters = [];
 
+  thisBookList.filterList.addEventListener('click', function(event){
+
+    const checkbox = event.target;
+    
+    if(checkbox.tagName == 'INPUT' && checkbox.type == 'checkbox' && checkbox.name == 'filter'){
+      if(checkbox.checked){
+        filters.push(checkbox.value);
+      } else {
+        filters.splice(filters.indexOf(checkbox.value), 1);
+      }
+      thisBookList.filterBooks(filters);
+    }
+  });
+}
 initActions();
+
+function filterBooks(filters){
+
+  for (const book of dataSource.books){
+  
+    const bookImage = document.querySelector('book__image[data-id="' + book.id + '"]');
+
+    let shouldBeHidden = false;
+
+    for (const filter of filters){
+      if(!book.details[filter]){
+        shouldBeHidden = true;
+        break;
+      } else {
+        shouldBeHidden = false;
+      }
+    }
+    if (shouldBeHidden){
+      bookImage.classList.add('hidden');
+    } else {
+      bookImage.classList.remove('hidden');
+    }
+  }
+}
+filterBooks();
